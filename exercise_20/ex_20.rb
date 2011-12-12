748df91f2b7abf66c86d24074b1231ee2f743491
btpcn <blacktulip+learn@gmail.com>
2011-12-12 10:36:00 +0000

    added a block comment of io#seek in exercise 20

--- exercise_20/ex_20.rb
+++ exercise_20/ex_20.rb
@@ -11,6 +11,27 @@ def rewind(f)
 	f.seek(0, IO::SEEK_SET)
 end
 
+=begin
+Method: IO#seek
+Defined in: io.c
+- (0) seek(amount, whence = IO::SEEK_SET)
+
+Seeks to a given offset anInteger in the stream according to the value of whence:
+
+  IO::SEEK_CUR  | Seeks to _amount_ plus current position
+  --------------+----------------------------------------------------
+  IO::SEEK_END  | Seeks to _amount_ plus end of stream (you probably
+                | want a negative value for _amount_)
+  --------------+----------------------------------------------------
+  IO::SEEK_SET  | Seeks to the absolute location given by _amount_
+Example:
+
+   f = File.new("testfile")
+   f.seek(-13, IO::SEEK_END)   #=> 0
+   f.readline                  #=> "And so on...\n"
+
+=end
+
 # define a function that print a line which the "pointer" is in of a file
 def print_a_line(line_count, f)
 	puts "#{line_count} #{f.readline()}"
@@ -37,3 +58,4 @@ print_a_line(current_line, current_file)
 
 current_line = current_line + 1
 print_a_line(current_line, current_file)
+
