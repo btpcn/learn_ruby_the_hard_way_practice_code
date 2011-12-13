84efb5b8959a4f80bb34942f0b61c690666f5dcd
btpcn <blacktulip+learn@gmail.com>
2011-12-13 15:54:31 +0000

    third ammendment

--- exercise_26/ex_26.rb
+++ exercise_26/ex_26.rb
@@ -1,47 +1,47 @@
-module ex25
+
 # This function will break up words for us.
-    def break_words(stuff)
-        words = stuff.split(' ')
-        return words
-    end
-
-    # Sorts the words.
-    def sort_words(words)
-        return words.sort()
-    end
-
-    # Prints the first word after popping it off.
-    def puts_first_word(words)
-        word = words.pop(0)
-        puts word
-    end
-
-    # Prints the last word after popping it off.
-    def puts_last_word(words)
-        word = words.pop(-1)
-        puts word
-    end
-
-    # Takes in a full sentence and returns the sorted words.
-    def sort_sentence(sentence)
-        words = break_words(sentence)
-        return sort_words(words)
-    end
-
-    # Puts the first and last words of the sentence.
-    def puts_first_and_last(sentence)
-        words = break_words(sentence)
-        puts_first_word(words)
-        puts_last_word(words)
-    end
-
-    # Sorts the words then prints the first and last one.
-    def puts_first_and_last_sorted(sentence)
-        words = sort_sentence(sentence)
-        puts_first_word(words)
-        puts_last_word(words)
-    end
+def break_words(stuff)
+    words = stuff.split(' ')
+    return words
 end
+
+# Sorts the words.
+def sort_words(words)
+    return words.sort()
+end
+
+# Prints the first word after popping it off.
+def puts_first_word(words)
+    word = words.pop(0)
+    puts word
+end
+
+# Prints the last word after popping it off.
+def puts_last_word(words)
+    word = words.pop(-1)
+    puts word
+end
+
+# Takes in a full sentence and returns the sorted words.
+def sort_sentence(sentence)
+    words = break_words(sentence)
+    return sort_words(words)
+end
+
+# Puts the first and last words of the sentence.
+def puts_first_and_last(sentence)
+    words = break_words(sentence)
+    puts_first_word(words)
+    puts_last_word(words)
+end
+
+# Sorts the words then prints the first and last one.
+def puts_first_and_last_sorted(sentence)
+    words = sort_sentence(sentence)
+    puts_first_word(words)
+    puts_last_word(words)
+end
+
 
 
 puts "Let's practice everything."
@@ -85,14 +85,14 @@ puts "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
 
 sentence = "All good things come to those who wait."
 
-words = ex25.break_words(sentence)
-sorted_words = ex25.sort_words(words)
+words = break_words(sentence)
+sorted_words = sort_words(words)
 
 puts_first_word(words)
 puts_last_word(words)
 puts_first_word(sorted_words)
 puts_last_word(sorted_words)
-sorted_words = ex25.sort_sentence(sentence)
+sorted_words = sort_sentence(sentence)
 print sorted_words
 
 puts_first_and_last(sentence)
